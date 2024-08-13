3af52f98ad2d1049aaeb2f235719f21efc614a6f
wisieeen <wisieeen@gmail.com>
2024-08-14 00:53:44 +0300

    synchro update
    Problem of desynchronization solved.
    Node-red flow updated accordingly.
    Added text input for JSON string that programs pumps.

--- infusion pumps/syringe_pump_2/syringe_pump_2.ino
+++ infusion pumps/syringe_pump_2/syringe_pump_2.ino
@@ -9,6 +9,7 @@ note that in program, steps must be multipled by 2
 #include <credentials.h>
 #include <PubSubClient.h>
 #include <WiFi.h>
+#include "ArduinoJson.h"
 
 const char* mqtt_server = "10.0.0.4";
 WiFiClient esp_pump_2;
@@ -20,14 +21,20 @@ const byte pin_1_e = 27; //endstop pin
 const byte pin_1_com = 32; //pin for communication between boards
 
 int speed_1 = 70; //70 is minimum
-int loop_num = 0;
+int block_num = 0;
+const int block_size = 10;
 int phase_1 = 0;
 unsigned long timer_1 = 0;
-unsigned long next_step_1 = 0;
+unsigned long next_step_1 = 0; //when stepper moves
+unsigned long block_start = 0; //when program block started
+unsigned long block_stop = 0;
 long absolute_pos_1 = 0;
 long requested_pos_1 = 0;
 int direction_1 = 1;
-
+int program_length = 0;
+JsonDocument program_1; //program buffer ["step1":["delay":70,"micros":1000],"step2":...]
+JsonDocument program_0;//executed program
+bool run_program_switch = 0;
 /*=====================
 function declarations for connectivity
 =====================*/
@@ -59,17 +66,17 @@ void callback(char* topic, byte* payload, unsigned int length) {
   }
   String pp= (char*)p;
   pp = pp.substring(0,length); //w/o this some weird data is added to stirng
-  /*Serial.print("Message arrived on topic: ");
+  Serial.print("Message arrived on topic: ");
   Serial.print(topic);
   Serial.print("; length: ");
   Serial.print(length);
   Serial.print("; payload: ");
-  Serial.println(pp);*/
+  Serial.println(pp);
 
-  if (ttopic=="pump_2/speed"){
+  if (ttopic=="pump_2/speed"){ //actually time distance between steps
     speed_1 = pp.toInt();
-    //Serial.print("pump/speed activated: ");
-    //Serial.println(speed_1);
+    Serial.print("pump/speed activated: ");
+    Serial.println(speed_1);
   }
 
   else if (ttopic=="pump_2/steps"){
@@ -85,7 +92,6 @@ void callback(char* topic, byte* payload, unsigned int length) {
     next_step_1 = micros();
   }
   
-
   else if (ttopic=="pump_2/end_position"){
     requested_pos_1 = pp.toInt()*2;
 
@@ -101,21 +107,46 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
   else if (ttopic=="pump_2/run"){
     next_step_1 = micros();
-    //digitalWrite(pin_1_com, 1);
-    //Serial.print("pump will run in next_step_1: ");
-    //Serial.println(next_step_1);
+    Serial.print("pump will run in next_step_1: ");
+    Serial.println(next_step_1);
+  }
+
+  else if (ttopic=="pump_2/run_program"){
+    program_0 = program_1;
+    speed_1 = int(program_0["0"][0]);
+    if (speed_1 <0){
+      direction_1 = 1;
+      digitalWrite(pin_1_d,1);
+    }
+    else{
+      direction_1 = -1;
+      digitalWrite(pin_1_d,0);
+    }
+    speed_1 = abs(speed_1);
+    run_program_switch = 1;
+    block_num = 1;
+    //client.publish("pump_master/program_request","1");
+    Serial.println("pump will run program");
+    while(!digitalRead(pin_1_com)){
+      ;
+    }
+    next_step_1 = micros();
+    block_start = next_step_1;
+    block_stop = block_start + int(program_0["0"][1]);
+  }
+  else if (ttopic=="pump_2/program"){
+    deserializeJson(program_1, pp); //Parse message
+    //Serial.print("program received: ");
+    //Serial.println(int(program_1["0"][0]));
   }
 
   else if (ttopic=="pump_2/homing"){
     homing();
   }
-  else if (ttopic=="ping"){
-    client.publish("common/ping",pp.c_str());
-  }
 
-  //else if (ttopic=="pump_master/step_done"){
-  //  digitalWrite(pin_1_com,1);
-  //}
+  else if (ttopic=="pump_2/program_length"){
+    program_length = pp.toInt();
+  }
 }
 
 void reconnect() {
@@ -133,10 +164,11 @@ void reconnect() {
       client.subscribe("pump_2/speed");
       client.subscribe("pump_2/steps");
       client.subscribe("pump_2/end_position");
+      client.subscribe("pump_2/program");
+      client.subscribe("pump_2/run_program");
       client.subscribe("pump_2/run");
       client.subscribe("pump_2/homing");
-      client.subscribe("ping");
-      //client.subscribe("pump_master/step_done");
+      client.subscribe("pump_2/program_length");
 
     } else {
       Serial.print("failed, rc=");
@@ -154,27 +186,90 @@ function declarations for pumping
 
 void pump_service(){
   unsigned long tn = micros();
-  if(tn>=next_step_1){
+  if(run_program_switch == 1){ //checks if it is program run
+    //Serial.println("program runs");
+    if(tn <= block_stop){ //checks if block isn't done
+      //Serial.println("block continues");
+      if(tn>=next_step_1){ //checks if next step now
+        //Serial.println("next step");
+        phase_1 = abs(phase_1-1); //single step
+        digitalWrite(pin_1_s,phase_1);
+        next_step_1 += speed_1;
+        absolute_pos_1 += direction_1;
+      } //end of this step
+    }//end of present block
+    else{ //if block is done
+      //Serial.println("next block");
+      if (block_num >= program_length){//checks if it was last block
+        //Serial.println("last block done");
+        next_step_1 = long_max;
+        run_program_switch = 0;
+        char cstr[16];
+        itoa(absolute_pos_1/2, cstr, 10);        
+        client.publish("pump_2/position_now",cstr);
+      }
+      else{
+        block_start = tn;
+        int block_num_mod = block_num % 10;
+        /*Serial.print("block_num_mod: ");
+        Serial.print(block_num_mod);
+        Serial.print(" block_start: ");
+        Serial.print(block_start);*/
+        block_stop = block_start + int(program_0[String(block_num)][1]);
+        speed_1 = int(program_0[String(block_num)][0]);
+        /*Serial.print(" block_stop: ");
+        Serial.print(block_stop);
+        Serial.print(" delta t: ");
+        Serial.print(block_stop-block_start);
+        Serial.print(" speed1: ");
+        Serial.println(speed_1);*/
+        if (speed_1 <0){//sets direction
+          direction_1 = 1;
+          digitalWrite(pin_1_d,1);
+        }
+        else{
+          direction_1 = -1;
+          digitalWrite(pin_1_d,0);
+        }
+        speed_1 = abs(speed_1);
+        if (block_num_mod == (char)(block_size-1)){ //if block number divisible by 9, then replace old buffer with new one
+          program_0 = program_1;
+          //Serial.println("buffer swap");
+          //serializeJson(program_0, Serial);
+          //client.publish("pump_master/program_request","1");
+        }
+        else if(block_num_mod == 1){ //ask for next village (of blocks)
+          //Serial.println("buffer request");
+          client.publish("pump_2/program_request","1");
+        }
+        block_num++;
+      }
+    }
+  } //end of program run
     
+  else if(tn>=next_step_1){
     if(absolute_pos_1 == requested_pos_1){ 
       //if goal reached, write timing info and sets next step to distant number
-      
       char cstr[16];
       itoa(absolute_pos_1/2, cstr, 10);
       client.publish("pump_2/position_now",cstr);
-      //digitalWrite(pin_1_com,0);
-      next_step_1 = long_max;
-      }
+      //if (series_length >0){
+      //  series_length--;
+      //  while (digitalRead(pin_1_com)==1){;}
+      //  client.publish("pump_master/step_done","Y");
+      //}
+      next_step_1 = long_max;}
     else{
       phase_1 = abs(phase_1-1);
       //Serial.print(phase_1);
       digitalWrite(pin_1_s,phase_1);
       next_step_1 += speed_1;
       absolute_pos_1 += direction_1;
+      
     }
   }
 }
-
+//client.publish("pump_master/step_done","Y");
 void homing(){
   digitalWrite(pin_1_d,1);
   for(int i=0; i<6000;i++){ //initial retract, just in case
@@ -205,9 +300,8 @@ void homing(){
     delayMicroseconds(500);
   }
   absolute_pos_1 = 0;
-  Serial.println("homing done");
+  client.publish("pump_2/position_now","0");
 }
-
 
 /*=====================
 program executions
@@ -218,7 +312,6 @@ void setup(){
   pinMode(pin_1_d, OUTPUT);
   pinMode(pin_1_e, INPUT_PULLUP);
   pinMode(pin_1_com, INPUT);
-  //digitalWrite(pin_1_com,1);
   Serial.begin(2000000);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
@@ -226,12 +319,10 @@ void setup(){
   timer_1=micros();
 }
 
-void loop()
-{
-  if (!client.connected()) { //23 us
+void loop(){
+  if (!client.connected()) {
     reconnect();
   }
-  client.loop(); //42 us
-  pump_service(); //<1 us 
-
+  client.loop();
+  pump_service();
 }
--- infusion pumps/syringe_pump_master/syringe_pump_master.ino
+++ infusion pumps/syringe_pump_master/syringe_pump_master.ino
@@ -9,6 +9,7 @@ note that in program, steps must be multipled by 2
 #include <credentials.h>
 #include <PubSubClient.h>
 #include <WiFi.h>
+#include "ArduinoJson.h"
 
 const char* mqtt_server = "10.0.0.4";
 WiFiClient esp_pump_3;
@@ -20,15 +21,20 @@ const byte pin_1_e = 27; //endstop pin
 const byte pin_1_com = 32; //pin for communication between boards
 
 int speed_1 = 70; //70 is minimum
-int loop_num = 0;
+int block_num = 0;
+const int block_size = 10;
 int phase_1 = 0;
 unsigned long timer_1 = 0;
-unsigned long next_step_1 = 0;
+unsigned long next_step_1 = 0; //when stepper moves
+unsigned long block_start = 0; //when program block started
+unsigned long block_stop = 0;
 long absolute_pos_1 = 0;
 long requested_pos_1 = 0;
 int direction_1 = 1;
-//int series_length = 0;
-
+int program_length = 0;
+JsonDocument program_1; //program buffer ["step1":["delay":70,"micros":1000],"step2":...]
+JsonDocument program_0;//executed program
+bool run_program_switch = 0;
 /*=====================
 function declarations for connectivity
 =====================*/
@@ -67,13 +73,13 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("; payload: ");
   Serial.println(pp);
 
-  if (ttopic=="pump_3/speed"){
+  if (ttopic=="pump_master/speed"){ //actually time distance between steps
     speed_1 = pp.toInt();
     Serial.print("pump/speed activated: ");
     Serial.println(speed_1);
   }
 
-  else if (ttopic=="pump_3/steps"){
+  else if (ttopic=="pump_master/steps"){
     requested_pos_1 += pp.toInt()*2;
     if(requested_pos_1 >absolute_pos_1){
       direction_1 = 1;
@@ -86,8 +92,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
     next_step_1 = micros();
   }
   
-
-  else if (ttopic=="pump_3/end_position"){
+  else if (ttopic=="pump_master/end_position"){
     requested_pos_1 = pp.toInt()*2;
 
     if(requested_pos_1 >absolute_pos_1){
@@ -100,18 +105,47 @@ void callback(char* topic, byte* payload, unsigned int length) {
     }
   }
 
-  else if (ttopic=="pump_3/run"){
+  else if (ttopic=="pump_master/run"){
     next_step_1 = micros();
     Serial.print("pump will run in next_step_1: ");
     Serial.println(next_step_1);
   }
 
-  else if (ttopic=="pump_3/homing"){
+  else if (ttopic=="pump_master/run_program"){
+    program_0 = program_1;
+    speed_1 = int(program_0["0"][0]);
+    if (speed_1 <0){
+      direction_1 = 1;
+      digitalWrite(pin_1_d,1);
+    }
+    else{
+      direction_1 = -1;
+      digitalWrite(pin_1_d,0);
+    }
+    speed_1 = abs(speed_1);
+    run_program_switch = 1;
+    block_num = 1;
+    //client.publish("pump_master/program_request","1");
+    Serial.println("pump will run program");
+    delay(1000);
+    digitalWrite(pin_1_com,1); //starts "on" signal
+    next_step_1 = micros();
+    block_start = next_step_1;
+    block_stop = block_start + int(program_0["0"][1]);
+  }
+  else if (ttopic=="pump_master/program"){
+    deserializeJson(program_1, pp); //Parse message
+    //Serial.print("program received: ");
+    //Serial.println(int(program_1["0"][0]));
+  }
+
+  else if (ttopic=="pump_master/homing"){
     homing();
   }
-  //else if (ttopic=="pump_master/series_length"){
-  //  series_length = pp.toInt();
-  //}
+
+  else if (ttopic=="pump_master/program_length"){
+    program_length = pp.toInt();
+  }
 }
 
 void reconnect() {
@@ -122,16 +156,18 @@ void reconnect() {
     if (client.connect("esp_pump_3")) {
       Serial.println("connected");
       // Once connected, publish an announcement...
-      client.publish("common/status","syringe pump_3 connected");
+      client.publish("common/status","syringe pump_master connected");
       char* a = (char*)absolute_pos_1;
-      client.publish("pump_3/position_now",a);
+      client.publish("pump_master/position_now",a);
       // ... and resubscribe
-      client.subscribe("pump_3/speed");
-      client.subscribe("pump_3/steps");
-      client.subscribe("pump_3/end_position");
-      client.subscribe("pump_3/run");
-      client.subscribe("pump_3/homing");
-      //client.subscribe("pump_master/series_length");
+      client.subscribe("pump_master/speed");
+      client.subscribe("pump_master/steps");
+      client.subscribe("pump_master/end_position");
+      client.subscribe("pump_master/program");
+      client.subscribe("pump_master/run_program");
+      client.subscribe("pump_master/run");
+      client.subscribe("pump_master/homing");
+      client.subscribe("pump_master/program_length");
 
     } else {
       Serial.print("failed, rc=");
@@ -149,12 +185,74 @@ function declarations for pumping
 
 void pump_service(){
   unsigned long tn = micros();
-  if(tn>=next_step_1){
+  if(run_program_switch == 1){ //checks if it is program run
+    //Serial.println("program runs");
+    if(tn <= block_stop){ //checks if block isn't done
+      //Serial.println("block continues");
+      if(tn>=next_step_1){ //checks if next step now
+        //Serial.println("next step");
+        phase_1 = abs(phase_1-1); //single step
+        digitalWrite(pin_1_s,phase_1);
+        next_step_1 += speed_1;
+        absolute_pos_1 += direction_1;
+      } //end of this step
+    }//end of present block
+    else{ //if block is done
+      //Serial.println("next block");
+      if (block_num >= program_length){//checks if it was last block
+        //Serial.println("last block done");
+        next_step_1 = long_max;
+        run_program_switch = 0;
+        char cstr[16];
+        itoa(absolute_pos_1/2, cstr, 10);        
+        digitalWrite(pin_1_com,0); //turns off start signal
+        client.publish("pump_master/position_now",cstr);
+      }
+      else{
+        block_start = tn;
+        int block_num_mod = block_num % 10;
+        /*Serial.print("block_num_mod: ");
+        Serial.print(block_num_mod);
+        Serial.print(" block_start: ");
+        Serial.print(block_start);*/
+        block_stop = block_start + int(program_0[String(block_num)][1]);
+        speed_1 = int(program_0[String(block_num)][0]);
+        /*Serial.print(" block_stop: ");
+        Serial.print(block_stop);
+        Serial.print(" delta t: ");
+        Serial.print(block_stop-block_start);
+        Serial.print(" speed1: ");
+        Serial.println(speed_1);*/
+        if (speed_1 <0){//sets direction
+          direction_1 = 1;
+          digitalWrite(pin_1_d,1);
+        }
+        else{
+          direction_1 = -1;
+          digitalWrite(pin_1_d,0);
+        }
+        speed_1 = abs(speed_1);
+        if (block_num_mod == (char)(block_size-1)){ //if block number divisible by 9, then replace old buffer with new one
+          program_0 = program_1;
+          //Serial.println("buffer swap");
+          //serializeJson(program_0, Serial);
+          //client.publish("pump_master/program_request","1");
+        }
+        else if(block_num_mod == 1){ //ask for next village (of blocks)
+          //Serial.println("buffer request");
+          client.publish("pump_master/program_request","1");
+        }
+        block_num++;
+      }
+    }
+  } //end of program run
+    
+  else if(tn>=next_step_1){
     if(absolute_pos_1 == requested_pos_1){ 
       //if goal reached, write timing info and sets next step to distant number
       char cstr[16];
       itoa(absolute_pos_1/2, cstr, 10);
-      client.publish("pump_3/position_now",cstr);
+      client.publish("pump_master/position_now",cstr);
       //if (series_length >0){
       //  series_length--;
       //  while (digitalRead(pin_1_com)==1){;}
@@ -202,8 +300,8 @@ void homing(){
     delayMicroseconds(500);
   }
   absolute_pos_1 = 0;
+  client.publish("pump_master/position_now","0");
 }
-
 
 /*=====================
 program executions
@@ -213,7 +311,7 @@ void setup(){
   pinMode(pin_1_s, OUTPUT);
   pinMode(pin_1_d, OUTPUT);
   pinMode(pin_1_e, INPUT_PULLUP);
-  pinMode(pin_1_com, INPUT);
+  pinMode(pin_1_com, OUTPUT);
   Serial.begin(2000000);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
